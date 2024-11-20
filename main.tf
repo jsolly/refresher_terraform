3196b0744c2b2870c053c4ffa398f2db5382a24e
John Solly <9572232+jsolly@users.noreply.github.com>
2024-11-20 09:45:27 -0500

    wip

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-052cef0575730ddd5"
+  instance_type = "t2.micro"
+}
